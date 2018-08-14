abff32e49145d081b52d8bcf6429d7615c515bde
5habeeb <ahmadshabeeb93@gmail.com>
2018-08-14 11:22:38 +0200

    add skeleton code

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const jsx = (
+  <div>
+    Hello world!
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
