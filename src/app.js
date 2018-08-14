78da2a448227b757abfee2e58f714afa8af64490
5habeeb <ahmadshabeeb93@gmail.com>
2018-08-14 16:40:44 +0200

    add popup

--- src/app.js
+++ src/app.js
@@ -4,12 +4,14 @@ import './styles/styles.scss';
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
+import Popup from './components/popup';
 
 const jsx = (
   <div>
     <Header />
     <Main />
     <Footer />
+    <Popup />
   </div>
 );
 
