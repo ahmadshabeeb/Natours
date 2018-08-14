86ed1c6745c7f8b110b402662cb2f34842bcd935
5habeeb <ahmadshabeeb93@gmail.com>
2018-08-14 16:36:36 +0200

    add footer

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 const jsx = (
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 );
 
